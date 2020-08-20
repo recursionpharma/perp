39e1e2d60b8de18986b5268c3bd34b8c0e561743
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-08-20 17:04:18 -0600

    Be explicit about conda lock generation

--- generate-locks.sh
+++ generate-locks.sh
@@ -25,7 +25,7 @@ fi
 
 docker image inspect $docker_image &> /dev/null || docker build . --build-arg PROJECT_DIR=$project_dir --tag $docker_image &> logs/docker.log
 
-docker run $docker_image -c "/test/bootstrap-conda.sh &> /dev/null && conda activate test &> /dev/null && conda env export" > $project_lock_dir/environment-lock.yml
+docker run $docker_image -c "/test/bootstrap-conda.sh &> /dev/null && ~/miniconda/bin/conda activate test &> /dev/null && ~/miniconda/bin/conda env export" > $project_lock_dir/environment-lock.yml
 docker run $docker_image -c "/test/bootstrap-pipenv.sh &> /dev/null && cat Pipfile.lock" > $project_lock_dir/Pipfile.lock
 docker run $docker_image -c "/test/bootstrap-poetry.sh &> /dev/null && cat poetry.lock" > $project_lock_dir/poetry.lock
 docker run $docker_image -c "/test/bootstrap-pip-compile.sh &> /dev/null && cat requirements.txt" > $project_lock_dir/requirements.txt
