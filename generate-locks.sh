f3346d49816759412df751eceaf227654f3acd84
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-08-20 14:40:01 -0600

    Add a lock generating script

---
+++ generate-locks.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+# set -e
+set -x
+
+if [[ $# < 1 ]];
+then
+    echo "$0 project"
+    exit -1
+fi
+
+project=$1
+project_dir=projects/$project
+project_lock_dir=$project_dir/lockfiles
+docker_image=profile-$project
+
+if [[ ! -d logs ]];
+then
+    mkdir logs
+fi
+
+if [[ ! -d $project_lock_dir ]];
+then
+    mkdir $project_lock_dir
+fi
+
+if docker image inspect $docker_image &> /dev/null; then
+    docker build . --build-arg PROJECT_DIR=$project_dir --tag $docker_image &> logs/docker.log
+fi
+docker run $docker_image -c "/test/bootstrap-conda.sh &> /dev/null && conda activate test &> /dev/null && conda env export" > $project_lock_dir/environment-lock.yml
+docker run $docker_image -c "/test/bootstrap-pipenv.sh &> /dev/null && cat Pipfile.lock" > $project_lock_dir/Pipfile.lock
+docker run $docker_image -c "/test/bootstrap-poetry.sh &> /dev/null && cat poetry.lock" > $project_lock_dir/poetry.lock
+docker run $docker_image -c "/test/bootstrap-pip-compile.sh &> /dev/null && cat requirements.txt" > $project_lock_dir/requirements.txt
