5f54e26a98ea7cfe1dae346030d0967297b46d30
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-08-21 15:03:57 -0600

    Fix error in pipenv version replacement

--- bootstrap/bootstrap-pipenv-skip-lock.sh
+++ bootstrap/bootstrap-pipenv-skip-lock.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sed -i"" -e "s/python_version =.*/python_version = $PY_VERSION/" Pipfile
+sed -i"" -e "s/python_version =.*/python_version = \"$PY_VERSION\"/" Pipfile
 PIPENV_IGNORE_VIRTUALENVS=1 ~/.local/bin/pipenv install --skip-lock
