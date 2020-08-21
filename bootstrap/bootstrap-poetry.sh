f8e0e0ea7e3198b83193c3604f4c105858e9ddbb
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-08-21 14:43:11 -0600

    Use pyenv for poetry python version

--- bootstrap/bootstrap-poetry.sh
+++ bootstrap/bootstrap-poetry.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 sed -i"" -e "s/python = .*/python = \"$PY_VERSION\"/" pyproject.toml
+eval "$(/root/.pyenv/bin/pyenv init -)"
+/root/.pyenv/bin/pyenv shell $PY_VERSION
 /root/.poetry/bin/poetry install --no-root
