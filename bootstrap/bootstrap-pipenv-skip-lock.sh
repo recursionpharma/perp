68b43b233547624b8fa0587aeb4b5d73c4993a12
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-08-23 05:52:21 -0600

    Set more env vars to keep pipenv from complaining

--- bootstrap/bootstrap-pipenv-skip-lock.sh
+++ bootstrap/bootstrap-pipenv-skip-lock.sh
@@ -2,6 +2,8 @@
 
 sed -i"" -e "s/python_version =.*/python_version = \"$PY_VERSION\"/" Pipfile
 
+export LC_ALL='en_US.UTF-8'
+export LANG='en_US.UTF-8'
 export PIPENV_NOSPIN=1
 export PIPENV_IGNORE_VIRTUALENVS=1
 export PIPENV_TIMEOUT=900
