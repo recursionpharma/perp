97aaced93be9c188fb39d6ee41125557132ea50e
Daniel Maljovec <daniel.maljovec@recursionpharma.com>
2020-08-24 06:53:01 -0600

    Match lines more specifically with poetry input file

--- bootstrap/bootstrap-poetry.sh
+++ bootstrap/bootstrap-poetry.sh
@@ -2,7 +2,7 @@
 
 export PATH="/root/.local/bin:/root/.pyenv/bin:/root/.poetry/bin:$PATH"
 
-sed -i"" -e "s/python = .*/python = \"$PY_VERSION\"/" pyproject.toml
+sed -i"" -e "s/^python = .*/python = \"$PY_VERSION\"/" pyproject.toml
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
