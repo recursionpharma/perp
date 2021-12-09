e63c8238b1a88dc71ee9fd4b2c7aa81374a89b82
Ayla Khan <ayla.khan@recursionpharma.com>
2021-12-09 13:49:11 -0700

    Add wheel to pip install step

--- bootstrap/bootstrap-pip+pyenv.sh
+++ bootstrap/bootstrap-pip+pyenv.sh
@@ -11,5 +11,5 @@ pyenv virtualenv $PY_VERSION test
 pyenv deactivate
 pyenv activate test
 echo "$0: $(python --version)"
-pip install pip --upgrade
+pip install --upgrade pip wheel
 pip install --no-cache-dir --use-feature=2020-resolver -r requirements.in
